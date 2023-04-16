022a3d26e825e1727c8ebe9833a0a81d88408c6f
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-04-16 11:40:45 -0400

    update to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
-      <div className='absolute xs:bottom-31.5 flex justify-center w-full items-center' >
+      <div className='absolute xs:bottom-31.75 flex justify-center w-full items-center' >
         <a href="#about" >
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.dev
