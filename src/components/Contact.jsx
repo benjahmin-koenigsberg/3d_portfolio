aa17dc6f5ac2de8dc7535951e5a9b55989b700af
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-04-17 15:32:50 -0400

    finish globe 3d

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,7 +20,7 @@ const handleChange = (e) => {}
 const handleSubmit = (e) => {}
 
   return (
-    <div className="xl: mt-12 xl: flex-row flex-column-reverse flex gap-10 overflow-hidden">
+    <div className="xl: mt-12 xl: flex-row flex-col-reverse flex gap-10 overflow-hidden">
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
         className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
@@ -55,7 +55,7 @@ const handleSubmit = (e) => {}
           <label className="flex flex-col">
             <span className="text-white font-medium mb-4">Your Message</span>
             <textarea
-            rows="7"
+              rows="7"
               name="message"
               value={form.message}
               onChange={handleChange}
@@ -63,10 +63,18 @@ const handleSubmit = (e) => {}
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
             />
           </label>
-          <button type="submit" className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl" >
-            {loading? "Sending..." : "Send"}
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl">
+            {loading ? "Sending..." : "Send"}
           </button>
         </form>
+      </motion.div>
+
+      <motion.div variants={slideIn("right", "tween", 0.2, 1)}
+      className="xl: flex-1 xl: h-auto md: h-[550px] h-[350px]">
+
+<EarthCanvas />
       </motion.div>
     </div>
   );
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -50,7 +50,7 @@ useEffect( () => {
   <OrbitControls
    enableZoom={false}
    maxPolarAngle={Math.PI / 2}
-   minPolarAngle={Math.PI / 2}
+  //  minPolarAngle={Math.PI / 2}
    />
    <Computers isMobile={isMobile}/>
 </Suspense>
